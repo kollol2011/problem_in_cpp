a9053a91b5b7af2ccd5f0a0e47caae1a6155885e
Ashikur Zaman Kollol <zamankollol12@gmail.com>
2025-07-11 17:59:22 +0600

    Update Greater Average.cpp

--- Greater Average.cpp
+++ Greater Average.cpp
@@ -1,13 +1,12 @@
 #include <iostream>
 using namespace std;
-
 int main() {
 	// your code goes here
 	int T;
 	cin>>T;
-	for(int i=0;i<T;i++){
+	for(int i=0;i<T;i++){  
 	   int A,B,C;
-	    cin>>A>>B>>C;
+	    cin>>A>>B>>C;  //for each line
 	 double average=(A+B)/2.0;
 	   if(average>C){
 	       cout<<"YES\n";
@@ -16,4 +15,3 @@ int main() {
 	   }     
 	}
 }
-
